9bfa5dd1f1e91df9b74388af2230bca7c3499f84
Abi <abrahamthiao@gmail.com>
2021-01-19 17:13:30 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(3000);
+app.listen(process.env.PORT);
